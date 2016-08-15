2bd4174a4597cc69c82bd7d043a6ec0b98591f46
Gustavo Ribeiro <Guster@Gustavos-MacBook-Pro.local>
2016-08-15 14:42:02 -0600

    notes

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 var journal = ["DM13 Rocks"];
 var journal = ["word"];
 var journal = ["naptime"];
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfoldername
+touch newFileName.txt
+
+git clone
+git status
+git add filename.js
+git commit -m "message"
+git push
+
+*/
